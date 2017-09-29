bdfb2eded1d76c0e890c16858ac6e4b61b478378
Rodrigo Kassick <kassick@gmail.com>
2017-09-29 03:33:37 -0300

    Passes tests

--- MMML/TypeRegistry.cpp
+++ MMML/TypeRegistry.cpp
@@ -5,7 +5,7 @@
  *
  *         Version: 1.0
  *         Created: "Thu Sep 28 00:12:18 2017"
- *         Updated: "2017-09-28 21:29:49 kassick"
+ *         Updated: "2017-09-29 02:41:49 kassick"
  *
  *          Author: Rodrigo Kassick
  *
@@ -13,39 +13,58 @@
  ****************************************************************************/
 
 #include "mmml/TypeRegistry.H"
+
+#include <iostream>
+#include <sstream>
+using namespace std;
 using namespace mmml;
 
-Type::const_pointer TypeRegistry::add(Type::pointer& t)
+Type::const_pointer
+TypeRegistry::add(Type::pointer&& t)
 {
-    auto dup_it = type_storage_set.find(t);
-    if (dup_it != type_storage_set.end())
+    auto dup_it = name_registry.find(t->name());
+    if (dup_it != name_registry.end())
     {
+        if (output)
+            cout << "found equal at " << dup_it->second->to_string() << endl;
+
         t.reset();
-        return *dup_it;
+        return dup_it->second;
     }
 
     int id = next_id++;
 
+    if (output)
+        cout << "Adding2 " << t->to_string() << " with id " << id << endl;
+
     t->set_id(id);
-    registry[id] = t;
     name_registry[t->name()] = t;
+    registry[id] = t;
 
     return t;
 }
 
-Type::const_pointer TypeRegistry::add(Type::pointer&& t) {
-    auto dup_it = type_storage_set.find(t);
-    if (dup_it != type_storage_set.end())
+Type::const_pointer
+TypeRegistry::add(Type::pointer& t)
+{
+    auto dup_it = name_registry.find(t->name());
+    if (dup_it != name_registry.end())
     {
+        if (output)
+            cout << "found equal at " << dup_it->second->to_string() << endl;
+
         t.reset();
-        return *dup_it;
+        return dup_it->second;
     }
 
     int id = next_id++;
 
+    if (output)
+        cout << "Adding " << t->to_string() << " with id " << id << endl;
+
     t->set_id(id);
-    registry[id] = t;
     name_registry[t->name()] = t;
+    registry[id] = t;
 
     return t;
 }
@@ -54,9 +73,9 @@ Type::const_pointer
 TypeRegistry::find_by_type(Type::const_pointer type)
         const
 {
-    auto it = type_storage_set.find(type);
-    if (it != type_storage_set.cend())
-        return *it;
+    auto it = name_registry.find(type->name());
+    if (it != name_registry.cend())
+        return it->second;
 
     return nullptr;
 }
@@ -67,7 +86,7 @@ TypeRegistry::find_by_name(const string name)
     {
         auto it = name_registry.find(name);
         if (it != name_registry.cend())
-            return it->second.lock();
+            return it->second;
 
         return nullptr;
     }
@@ -82,3 +101,22 @@ TypeRegistry::find_by_id(const int id)
 
     return it->second.lock();
 }
+
+string TypeRegistry::to_string() const
+{
+    std::stringstream s;
+    s << endl;
+
+    for (const auto &p: name_registry)
+    {
+        s << "Name: " << p.first << " -> " << p.second->to_string() << endl;
+    }
+
+    for (const auto &p : registry)
+        if (p.second.lock())
+            s << "Id: " << p.first << " -> " << p.second.lock()->name() << endl;
+
+    return s.str();
+}
+
+int mmml::output = 0;
--- MMML/tests/test_types.cpp
+++ MMML/tests/test_types.cpp
@@ -5,7 +5,7 @@
  *
  *         Version: 1.0
  *         Created: "Wed Sep 27 21:53:47 2017"
- *         Updated: "2017-09-28 22:29:15 kassick"
+ *         Updated: "2017-09-29 03:32:53 kassick"
  *
  *          Author: Rodrigo Kassick
  *
@@ -18,6 +18,11 @@
 
 using namespace mmml;
 using namespace std;
+
+template <class T>
+shared_ptr<T> shared_from(T* ptr) {
+    return shared_ptr<T>(ptr);
+}
 
 int main(int argc, char *argv[])
 {
@@ -68,6 +73,106 @@ int main(int argc, char *argv[])
     cout << "Char == {int, char}? " << (ct->equals(tup3) ? "true" : "false")
          << " (should be false)"
          << endl;
+
+    auto& registry = TypeRegistry::instance();
+    registry.add(tup1);
+    registry.add(tup2);
+    registry.add(tup3);
+
+    auto s1 = make_shared<ClassType>("teste1",
+                                     ClassType::base_field_vector_t(
+                                         {
+                                             {"a", ct},
+                                             {"b", it}
+                                         }
+                                                                    ) );
+    auto s2 = make_shared<ClassType>("teste2",
+                                     ClassType::base_field_vector_t(
+                                         {
+                                             {"a", ct},
+                                             {"b", it}
+                                         }
+                                                                    ) );
+
+    auto s3 = make_shared<ClassType>("teste1",
+                                     ClassType::base_field_vector_t(
+                                         {
+                                             {"b", it},
+                                             {"a", ct},
+                                             {"d", ct},
+                                         }
+                                                                    ) );
+    cout << s1->to_string() << " == " << s2->to_string() << "? "
+         << (s1->equals(s2) ? "true" : "false")
+         << " (should be false)"
+         << endl;
+
+    cout << s1->to_string() << " ==equiv== " << s2->to_string() << "? "
+         << (s1->equivalent(s2) ? "true" : "false")
+         << " (should be true)"
+         << endl;
+
+    cout << s1->to_string() << " == " << s3->to_string() << "? "
+         << (s1->equals(s3) ? "true" : "false")
+         << " (should be true)"
+         << endl;
+
+    cout << s1->to_string() << " ==equiv== " << s3->to_string() << "? "
+         << (s1->equivalent(s3) ? "true" : "false")
+         << " (should be false)"
+         << endl;
+
+    cout << s1->to_string() << " == " << tup1->to_string() << "? "
+         << (s1->equals(tup1) ? "true" : "false")
+         << " (should be false)"
+         << endl;
+
+    cout << tup1->to_string() << " ==equiv== " << s1->to_string() << "? "
+         << (s1->equivalent(tup1) ? "true" : "false")
+         << " (should be false)"
+         << endl;
+
+    cout << tup1->to_string() << " ==storage== " << s1->to_string() << "? "
+         << (s1->equal_storage(tup1) ? "true" : "false")
+         << " (should be true)"
+         << endl;
+
+    registry.add(s1);
+    registry.add(s2);
+    cout << "After trying to register type again, got " << registry.add(s3)->to_string() << endl;
+
+    auto seq1 = make_shared<SequenceType>(it);
+    auto seq2 = make_shared<SequenceType>(ct);
+    auto seq3 = make_shared<SequenceType>(it);
+
+    cout << seq1->to_string() << " == " << seq2->to_string() << "? "
+         << (seq1->equals(seq2) ? "true" : "false")
+         << " (should be false)"
+         << endl;
+
+    cout << seq2->to_string() << " == " << seq1->to_string() << "? "
+         << (seq2->equals(seq1) ? "true" : "false")
+         << " (should be false)"
+         << endl;
+
+    cout << seq2->to_string() << " == " << seq2->to_string() << "? "
+         << (seq2->equals(seq2) ? "true" : "false")
+         << " (should be true)"
+         << endl;
+
+    cout << seq1->to_string() << " == " << seq3->to_string() << "? "
+         << (seq1->equals(seq2) ? "true" : "false")
+         << " (should be true)"
+         << endl;
+
+    registry.add(seq1);
+    registry.add(seq2);
+    cout << "Adding duplicated sequence " << seq3->to_string()
+         << " returns " << registry.add(seq3)->to_string()
+         << ", should be " << seq1->to_string()
+         << endl;
+
+    cout << registry.to_string();
 
 
     return 0;
