1ecd77a89b9ae3090534e71347b330e4ec995ae0
Rodrigo Kassick <kassick@gmail.com>
2017-10-23 14:25:47 -0200

    Make tuples accept compatibility of nil / sequence

--- MMML/examples/01-hello_world.s
+++
@@ -1,7 +0,0 @@
-start      :                                      # main function
-             push "Ola Mundo!"
-             acreate                              # type(str)
-             aload
-             prints
-l0         : print
-             exit
